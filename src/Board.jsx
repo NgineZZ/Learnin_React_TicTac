73daf3e8b933d8600260210487b7ab73bf00a15d
Faisal Kabir <faisal.kabir71@gmail.com>
2024-01-17 17:40:42 +0600

    modi fied // game winner found

--- src/Board.jsx
+++ src/Board.jsx
@@ -6,6 +6,37 @@ export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+  // helper function;
+  function calculateWiner(square) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if(square[a] && square[a] === square[b] && square[a] === square[c]) {
+        return square[a];
+      }
+    }
+    return null;
+  }
+
+  const winner = calculateWiner(squares);
+  let status;
+
+  if(winner){
+    status = "Winner: " + winner;
+  } else {
+    status = "Next Player: " + (xIsNext? "X" : "O");
+  }
+
   function handleSqureClick(i){
     if(squares[i]) {
       return;
@@ -23,6 +54,7 @@ export default function Board() {
 
   return(
     <>
+    <p>{status}</p>
     <div className="board-row">
       <Suaqre value={squares[0]} onSquareClick= {()=>handleSqureClick(0)}/>
       <Suaqre value={squares[1]} onSquareClick={()=>handleSqureClick(1)}/>
